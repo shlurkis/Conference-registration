d5905a830958f4407028c8213dcf6b80a164c00c
shlurkis <arminasrupsys03@gmail.com>
2024-10-20 19:25:50 +0300

    basic setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user', function () {
+    return view('user');
+});
+Route::get('/employee', function () {
+    return view('employee');
+});
+Route::get('/admin', function () {
+    return view('admin');
+});
