6f1fa55d39d4091b324cc08dc5783de5021b7cd2
shlurkis <arminasrupsys03@gmail.com>
2024-11-10 18:50:56 +0200

    updated the employee view of conferences

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -7,18 +7,36 @@
         @foreach($conferences as $conference)
             <li class="list-group-item">
                 <h5>{{ $conference->name }}</h5>
-                <p>Attendees:</p>
-                <ul>
-                    @if($conference->users->isEmpty())
-                        <li>No attendees yet.</li>
-                    @else
-                        @foreach($conference->users as $attendee)
-                            <li>{{ $attendee->name }} ({{ $attendee->email }})</li>
-                        @endforeach
-                    @endif
-                </ul>
+                <button class="btn btn-info btn-sm" onclick="toggleDetails({{ $conference->id }})">Show More</button>
+
+                <div id="conference-details-{{ $conference->id }}" class="conference-details" style="display: none; margin-top: 10px;">
+                    <p><strong>Description:</strong> {{ $conference->description }}</p>
+                    <p><strong>Date:</strong> {{ $conference->date }}</p>
+
+                    <p><strong>Attendees:</strong></p>
+                    <ul>
+                        @if($conference->users->isEmpty())
+                            <li>No attendees yet.</li>
+                        @else
+                            @foreach($conference->users as $attendee)
+                                <li>{{ $attendee->name }} ({{ $attendee->email }})</li>
+                            @endforeach
+                        @endif
+                    </ul>
+                </div>
             </li>
         @endforeach
     </ul>
 </div>
+
+<script>
+    function toggleDetails(conferenceId) {
+        var detailsDiv = document.getElementById('conference-details-' + conferenceId);
+        if (detailsDiv.style.display === "none") {
+            detailsDiv.style.display = "block";
+        } else {
+            detailsDiv.style.display = "none";
+        }
+    }
+</script>
 @endsection
